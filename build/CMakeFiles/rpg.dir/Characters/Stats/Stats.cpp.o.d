a8eb57134e88050e9b3771d87fb538035be1456c
awolschi <wolschinski@gmail.com>
2026-07-24 03:01:09 +0200

    Add .gitignore, remove tracked build artifacts

--- build/CMakeFiles/rpg.dir/Characters/Stats/Stats.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/rpg.dir/Characters/Stats/Stats.cpp.o: \
- /home/awolschi/Desktop/OwnProjects/RPG/Characters/Stats/Stats.cpp \
- /usr/include/stdc-predef.h \
- /home/awolschi/Desktop/OwnProjects/RPG/Characters/Stats/Stats.hpp
--- build/CMakeFiles/rpg.dir/Graphics/BattleLayout.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/rpg.dir/Graphics/BattleLayout.cpp.o: \
- /home/awolschi/Desktop/OwnProjects/RPG/Graphics/BattleLayout.cpp \
- /usr/include/stdc-predef.h \
- /home/awolschi/Desktop/OwnProjects/RPG/Graphics/BattleLayout.hpp
